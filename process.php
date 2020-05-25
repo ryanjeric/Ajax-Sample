2eec9ed5aa195f0f1d73bc76bb36aaf09df06c81
ryanjericsabado <ryanjericsabado@yahoo.com>
2020-05-25 19:10:20 +0800

    INIT

---
+++ process.php
@@ -0,0 +1,25 @@
+<?php
+
+//Connect to a database
+$conn = mysqli_connect('localhost','root','','ajaxtest');
+
+
+echo 'Processing....';
+// Check for GET Varaiable
+
+if(isset($_GET['name'])){
+    echo 'GET: Your name is '. $_GET['name'];
+}
+
+if(isset($_POST['name'])){
+    $name = mysqli_real_escape_string($conn,$_POST['name']);
+
+    echo 'POST: Your name is '. $_POST['name'];
+
+    $query ="INSERT INTO users(name) VALUES('$name')";
+    if(mysqli_query($conn,$query)){
+        echo 'User Added';
+    }else{
+        echo 'ERROR: '. mysqli_error($conn);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+# AXIOS
+> https://www.youtube.com/watch?v=82hnvUYY6QA
